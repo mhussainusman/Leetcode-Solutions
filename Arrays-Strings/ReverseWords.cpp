8629660f67232569f80f2ec98ba2984b8a05e2eb
Hussain Usman (RollNo: 25i_2067) <mhussainusman@gmail.com>
2026-06-24 07:46:06 +0500

    Reverse Words of a String sol added

---
+++ Arrays-Strings/ReverseWords.cpp
@@ -0,0 +1,34 @@
+// Approach:
+// Time: 
+// Space::
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string>words;
+        string word="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                word+=s[i];
+            }
+            else {
+                if ( word!=""){
+                    words.push_back(word);
+                    word="";
+                }
+
+            }
+        }
+        if(word!=""){words.push_back(word);
+        }
+        string result;
+        for(int i=words.size()-1;i>=0;i--){
+            result+=words[i];
+            if(i!=0)result+=" ";
+
+        }
+        return result;
+    }
+};
