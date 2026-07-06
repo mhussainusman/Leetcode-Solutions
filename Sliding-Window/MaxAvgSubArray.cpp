a20c250183632b2d3a4dd002bb59d0131e80ec21
Hussain Usman (RollNo: 25i_2067) <mhussainusman@gmail.com>
2026-07-06 07:29:40 +0500

    Max Avg Subarray solution added

--- Sliding-Window/MaxAvgSubArray.cpp
+++ Sliding-Window/MaxAvgSubArray.cpp
@@ -1,5 +1,22 @@
-// Approach:
+// Approach: Sliding Window
 // Time: O(n)
 // Space: O(1)
 using namespace std;
 #include<vector>
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double current=0;
+        int n= nums.size();
+        for(int i=0;i<k;i++){
+            current+=nums[i];
+        }
+        double max=current;
+        for(int j=1;j<=n-k;j++){
+            current=current-nums[j-1]+nums[j+k-1];
+            if (current>max){max=current;}
+
+        }
+        return max/k;
+    }
+};
